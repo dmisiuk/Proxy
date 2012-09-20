770c6df44fb0d2f1c9e33a18436c6df739e32851
minsler <minsler.by@gmail.com>
2012-09-20 16:21:09 +0300

    Implement proxy attachement and it work with text file, but not binary

--- src/main/java/minsler/by/ProxyServletToDisk.java
+++ src/main/java/minsler/by/ProxyServletToDisk.java
@@ -3,6 +3,7 @@ package minsler.by;
 import java.io.DataInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -19,80 +20,97 @@ public class ProxyServletToDisk extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		System.out.println("toDiskServlet: connected to servlet");
-		String message = "Файл загружен успешно!";
-		String err = "Ошибка при чтении файла - файл неопределен";
+
 		resp.setContentType("text/html;charset=utf-8");
-		System.out.println(req.getClass());
+		resp.setCharacterEncoding("utf-8");
+		req.setCharacterEncoding("utf-8");
+		// System.out.println(req.getClass());
 
 		PrintWriter out = resp.getWriter();
 
 		String contentType = req.getContentType();
 		ServletInputStream in = req.getInputStream();
 		DataInputStream dis = new DataInputStream(in);
-		// dis.mark(1024);
-		// InputStreamReader isr = new InputStreamReader(in);
-		// BufferedReader br = new BufferedReader(isr);
-		String line;
-		int lentghOfInfoContent = 0;
-		StringBuilder sb = new StringBuilder();
 
-		// while ((line = br.readLine()) != null && line.length() != 0) {
-		// System.out.println(line);
-		// lentghOfInfoContent += line.length() + 1;
-		// }
-		// lentghOfInfoContent++;
-		// System.out.println("line|" + line + "|....");
-		//
-		// dis.reset();
-		// // System.out.print("after");
-		//
-		// int length = req.getContentLength();
-		// System.out.println("toDiskServlet: length " + length);
-		// System.out.println("toDiskServlet: contentType " + contentType);
-		// String boundary = contentType.substring(contentType
-		// .indexOf("boundary=") + 9);
-		// System.out.println("toDiskServlet: boundary " + boundary);
-		//
-		// lentghOfInfoContent += boundary.length() + 5;
+		int contentLength = req.getContentLength();
+		System.out.println("toDiskServlet: length " + contentLength);
+		System.out.println("toDiskServlet: contentType " + contentType);
+		String boundary = contentType.substring(contentType
+				.indexOf("boundary=") + 9);
+		System.out.println("toDiskServlet: boundary " + boundary);
+
 		// System.out.println("readed byte: " + lentghOfInfoContent);
 
 		byte array[] = new byte[8 * 1024];
-		byte preArray[] = new byte[8 * 1024];
+		boolean isBodyAttachment = false;
+
+		StringBuilder sb = new StringBuilder();
 		int nread;
-		FileOutputStream fos = new FileOutputStream("uploadedByParts.in");
+		FileOutputStream fos = new FileOutputStream("attachment.part");
+		String contentIdPayload = "Content-ID: Payload-0" + "\n\n";
+		int indexFrom = 0;
+		int indexContentIdPayload = -1;
+
 		while ((nread = dis.read(array)) >= 0) {
-			// System.out.println("toDiskServlet: nread: " + nread);
-			// System.out.println("toDiskServlet: available: " +
-			// dis.available());
+			System.out.println("start search from:" + indexFrom);
+			System.out.println("toDisk: nread: " + nread);
+			// end of stream?
 			String substring = new String(array, 0, nread);
+			// System.out.println("substring: " + substring);
 			sb.append(substring);
-			System.out.print(substring);
-			fos.write(array, 0, nread);
+			if ((indexContentIdPayload = sb
+					.indexOf(contentIdPayload, indexFrom)) >= 0) {
+				isBodyAttachment = true;
+				break;
+			} else {
+				indexFrom = sb.length() - contentIdPayload.length() - 1;
+			}
 		}
-		System.out.println("bytes in sb:" + new String(sb).getBytes().length);
+
+		String endBoundary = "--" + boundary + "--" + "\n";
+		int endIndex = indexContentIdPayload + contentIdPayload.length();
+		int availableBytesForWrite = contentLength
+				- sb.substring(0, endIndex).getBytes().length
+				- endBoundary.getBytes().length;
+
+		availableBytesForWrite -= writeToOutput(fos, sb.substring(endIndex)
+				.getBytes(), availableBytesForWrite);
+
+		while ((nread = dis.read(array)) >= 0) {
+			// String substring = new String(array, 0, nread);
+			// System.out.print(substring);
+			// fos.write(array, 0, nread);
+			availableBytesForWrite -= writeToOutput(fos, array, 0, nread,
+					availableBytesForWrite);
+
+		}
+		System.out.println("isBodyAttachment: " + isBodyAttachment);
+		System.out.println("Index: " + indexContentIdPayload);
+
 		fos.flush();
 		fos.close();
-		System.out.println("charter in sb:" + sb.length());
-		// System.out.println("toDiskServlet: nread after while: " + nread);
-		// byte array[] = new byte[length];
-		// int dataRead = 0, totalData = 0;
-		//
-		// while (totalData < length) {
-		// System.out.println("available byte is " + dis.available());
-		// dataRead = dis.read(array, totalData, length);
-		// totalData += dataRead;
-		// System.out.println("total data = " + totalData + "| dataread = "
-		// + dataRead);
-		// }
-		//
-		// FileOutputStream fos1 = new FileOutputStream("uploadedfile.in");
-		// fos1.write(array);
-		// fos1.close();
 
 		out.println("<html>" + "<head>"
 				+ "<title>Загрузка файла на сервер</title>" + "</head>"
-				+ "<body bgcolor=#fff1df>" + "<h3>" + message + "</h3>"
+				+ "<body bgcolor=#fff1df>" + "<h3>" + "успешно" + "</h3>"
 				+ "</body>" + "</html>");
 		out.close();
 	}
+
+	private int writeToOutput(OutputStream out, byte[] b, int availableToWrite)
+			throws IOException {
+		return this.writeToOutput(out, b, 0, b.length, availableToWrite);
+	}
+
+	private int writeToOutput(OutputStream out, byte[] b, int startIndex,
+			int endIndex, int availableToWrite) throws IOException {
+		if (availableToWrite >= (endIndex - startIndex)) {
+			out.write(b, startIndex, endIndex);
+			return (endIndex - startIndex);
+		} else {
+			out.write(b, startIndex, startIndex + availableToWrite);
+			return availableToWrite;
+		}
+	}
+
 }
--- src/main/java/minsler/by/ProxyServletToOtherUrl.java
+++ src/main/java/minsler/by/ProxyServletToOtherUrl.java
@@ -2,7 +2,9 @@ package minsler.by;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
@@ -38,46 +40,121 @@ public class ProxyServletToOtherUrl extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		System.out.println("ProxyServlet: connected to servlet");
-		String message = "Файл загружен успешно!";
-		String err = "Ошибка при чтении файла - файл неопределен";
 		resp.setContentType("text/html;charset=utf-8");
+		resp.setCharacterEncoding("utf-8");
+		req.setCharacterEncoding("utf-8");
 
 		PrintWriter out = resp.getWriter();
 		String contentType = req.getContentType();
 		DataInputStream dis = new DataInputStream(req.getInputStream());
 		// if is multipart then
 
-		int length = req.getContentLength();
+		int contentLength = req.getContentLength();
+		String boundaryOfInputRequest = contentType.substring(contentType
+				.indexOf("boundary=") + 9);
+
+		byte array[] = new byte[8 * 1024];
+		boolean isBodyAttachment = false;
+		StringBuilder sb = new StringBuilder();
+		int nread;
+		FileOutputStream fosSoapFile = new FileOutputStream("uploaded-soap.xml");
+		String contentIdPayload = "Content-ID: Payload-0" + "\n\n";
+		int indexFrom = 0;
+		int indexContentIdPayload = -1;
+
+		while ((nread = dis.read(array)) >= 0) {
+			// System.out.println("start search from:" + indexFrom);
+			// System.out.println("toDisk: nread: " + nread);
+			// end of stream?
+			String substring = new String(array, 0, nread);
+			// System.out.println("substring: " + substring);
+			sb.append(substring);
+			if ((indexContentIdPayload = sb
+					.indexOf(contentIdPayload, indexFrom)) >= 0) {
+				isBodyAttachment = true;
+				break;
+			} else {
+				indexFrom = sb.length() - contentIdPayload.length() - 1;
+			}
+		}
+
+		String endBoundary = "--" + boundaryOfInputRequest + "--" + "\n";
+		int endIndex = indexContentIdPayload + contentIdPayload.length();
+		fosSoapFile.write(sb.substring(0, endIndex).getBytes());
+		fosSoapFile.flush();
+		fosSoapFile.close();
+
+		int availableBytesForWrite = contentLength
+				- sb.substring(0, endIndex).getBytes().length
+				- endBoundary.getBytes().length;
 
 		HttpURLConnection connection = (HttpURLConnection) receiverUrl
 				.openConnection();
+
 		connection.setDoOutput(true);
 		connection.setRequestMethod("POST");
 		// TO-DO boundary
+		String boundaryForNewRequest = generateBoundary();
+
 		connection.setRequestProperty("Content-Type",
-				"multipart/form-data; boundary=boundary.minsler.by");
-		connection.setFixedLengthStreamingMode(length);
+				"multipart/form-data; boundary=" + boundaryForNewRequest);
+
+		String attachmentHeaders = "--" + boundaryForNewRequest + "\n"
+				+ "Content-Type: text/plain; charset=\"utf-8\"\n"
+				+ "Content-ID: Payload-0\n\n";
+		String multipartEnd = "--" + boundaryForNewRequest + "--\n";
+
+		StringBuilder sbNewRequest = new StringBuilder();
+		sbNewRequest.append(attachmentHeaders).append(multipartEnd);
+		long lentghOfStream = availableBytesForWrite
+				+ new String(sbNewRequest).getBytes().length;
+
+		connection.setFixedLengthStreamingMode(lentghOfStream);
+
 		DataOutputStream dos = new DataOutputStream(
 				connection.getOutputStream());
 
-		byte array[] = new byte[8 * 1024];
-		int nread;
+		dos.writeBytes(attachmentHeaders);
+
+		availableBytesForWrite -= writeToOutput(dos, sb.substring(endIndex)
+				.getBytes(), availableBytesForWrite);
 
 		while ((nread = dis.read(array)) >= 0) {
-			System.out.println("ProxyServlet: nread: " + nread);
-			dos.write(array, 0, nread);
+			// String substring = new String(array, 0, nread);
+			// System.out.print(substring);
+			// fos.write(array, 0, nread);
+			availableBytesForWrite -= writeToOutput(dos, array, 0, nread,
+					availableBytesForWrite);
+
 		}
 
+		dos.writeBytes(multipartEnd);
 		dos.flush();
 		dos.close();
 		System.out.println("ProxyServlet: response code from toDisk servlet"
 				+ connection.getResponseCode());
 		connection.disconnect();
+		out.println("success");
+	}
 
-		out.println("<html>" + "<head>"
-				+ "<title>Загрузка файла на сервер</title>" + "</head>"
-				+ "<body bgcolor=#fff1df>" + "<h3>" + message + "</h3>"
-				+ "</body>" + "</html>");
-		out.close();
+	private static String generateBoundary() {
+		// TO-DO implement generation boundary
+		return "1newpartminslerbyboundary";
+	}
+
+	private int writeToOutput(OutputStream out, byte[] b, int availableToWrite)
+			throws IOException {
+		return this.writeToOutput(out, b, 0, b.length, availableToWrite);
+	}
+
+	private int writeToOutput(OutputStream out, byte[] b, int startIndex,
+			int endIndex, int availableToWrite) throws IOException {
+		if (availableToWrite >= (endIndex - startIndex)) {
+			out.write(b, startIndex, endIndex);
+			return (endIndex - startIndex);
+		} else {
+			out.write(b, startIndex, startIndex + availableToWrite);
+			return availableToWrite;
+		}
 	}
 }
